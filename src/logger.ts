70ffd18b211f6ec4713bf4807077899093fb2661
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2026-05-17 13:30:34 +0200

    refactor(logger): remove color and symbol dependencies from logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,3 @@
-import * as colors from "./terminal/colors.ts";
-import { errorSymbol, infoSymbol, warningSymbol } from "./terminal/symbols.ts";
-
 export type LogLevel = "log" | "debug" | "trace" | "info" | "warn" | "error";
 export type LoggerSink = (level: LogLevel, args: unknown[]) => void;
 
@@ -38,33 +35,6 @@ function writeLog(level: LogLevel, args: unknown[]): void {
 	if (loggerSink) {
 		loggerSink(level, args);
 		return;
-	}
-
-	if (colors.supportsColor) {
-		if (level === "debug") {
-			console.debug(colors.white(args));
-			return;
-		}
-
-		if (level === "trace") {
-			console.trace(colors.gray(args));
-			return;
-		}
-
-		if (level === "info") {
-			console.info(colors.blue([infoSymbol, ...args]));
-			return;
-		}
-
-		if (level === "warn") {
-			console.warn(colors.yellow([warningSymbol, ...args]));
-			return;
-		}
-
-		if (level === "error") {
-			console.error(colors.red([errorSymbol, ...args]));
-			return;
-		}
 	}
 
 	if (level === "log") {
